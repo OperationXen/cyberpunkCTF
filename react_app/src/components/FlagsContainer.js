55d99b26cf4c694c032a450176207578443e113d
Mike Hogarth <mikehogarth20@hotmail.com>
2019-07-13 18:13:08 +0100

    Promote to parent

--- react_app/src/components/FlagsContainer.js
+++ react_app/src/components/FlagsContainer.js
@@ -1,54 +1,56 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 
-import Grid from "@material-ui/core/grid";
-import Paper from "@material-ui/core/paper";
-import CTFCategory from "./CTFCategory";
+import Grid from '@material-ui/core/grid'
+import Paper from '@material-ui/core/paper'
+import CTFCategory from './CTFCategory'
 
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 const GET_ALL_CATEGORIES_QUERY = gql`
-  {
-    allCategories {
-      id
-      title
-      order
+    {
+      allCategories{
+        id
+        title
+        order
+      }
     }
-  }
 `;
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(0.3)
-  }
+  },
 }));
 
-export default function FlagsContainer(props) {
-  const classes = useStyles();
+export default function FlagsContainer(props){
+    const classes = useStyles();
 
-  return (
+  return(
     <div className={classes.root}>
-      <Grid
-        container
-        spacing={3}
-        direction="row"
-        justify="space-around"
-        alignItems="center"
-      >
-        <Query query={GET_ALL_CATEGORIES_QUERY} pollInterval={60000}>
-          {({ loading, error, data }) => {
-            if (loading) return <div>Fetching</div>;
-            if (error) return <div>Error</div>;
+        <Grid
+            container
+            spacing = {3}
+            direction="row"
+            justify="space-around"
+            alignItems="center"
+        >
+            <Query query={GET_ALL_CATEGORIES_QUERY} pollInterval={60000}>{
+                ({ loading, error, data }) => {
+                  if (loading) return <div>Fetching</div>
+                  if (error) return <div>Error</div>
 
-            return data.allCategories.map(category => (
-              <Grid item xs={6}>
-                <CTFCategory key={category.id} category={category} />
-              </Grid>
-            ));
-          }}
-        </Query>
-      </Grid>
+                  return (
+                      data.allCategories.map(category => 
+                        <Grid item xs={6}>
+                          <CTFCategory key={category.id} category={category} />
+                        </Grid>
+                      )
+                  )
+            }}
+            </Query>
+        </Grid>
     </div>
-  );
+  )
 }
