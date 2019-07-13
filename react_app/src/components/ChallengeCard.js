80ca9bb26f637f654d9cb23ffe14b6c22c141e05
OperationXen <OperationXen@apocryphasecurity.com>
2019-07-13 18:35:54 +0100

    Added chip to challenge card to store value

--- react_app/src/components/ChallengeCard.js
+++ react_app/src/components/ChallengeCard.js
@@ -1,27 +1,44 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
+import AddIcon from '@material-ui/icons/Add'
+
 import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
+import Chip from '@material-ui/core/Chip'
+import Avatar from '@material-ui/core/Avatar'
 import Divider from '@material-ui/core/Divider'
 
 const useStyles = makeStyles(theme => ({
   root: {
     margin: theme.spacing(),
   },
+  valueChip: {
+    marginLeft: "-60%",
+    marginTop: "-25%",
+  }
 }));
 
-export default function ChallengeCard(props){
-    const classes = useStyles()
+export default function ChallengeCard(props) {
+  const classes = useStyles()
 
-    return(
-        <Card className={classes.root}>
-            <CardContent>
-                <Typography>{props.title}</Typography>
-                    <Divider />
-            </CardContent>
-        </Card>
-    )
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Chip 
+          className={classes.valueChip} 
+          size="small" 
+          variant="outlined" 
+          label={props.points ? props.points:" --- "}
+          color="primary"
+          avatar={<Avatar><AddIcon /></Avatar>}
+        />
+        <Typography variant="h6">{props.title}</Typography>
+        <Divider />
+        <Typography variant="body2">{props.slug}</Typography>
+      </CardContent>
+    </Card>
+  )
 }
