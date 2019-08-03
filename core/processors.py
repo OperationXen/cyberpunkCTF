ade9e3fc4d57b41d893239b727c13332a3785080
OperationXen <OperationXen@apocryphasecurity.com>
2019-08-03 21:37:04 +0100

    Fleshed out flag submission logic

--- core/processors.py
+++ core/processors.py
@@ -1,20 +1,37 @@
 from core.models.challenges import Solve
 from core.models.flags import BaseFlag
 
+
 def check_submission(flag, submitted_flag):
+    """ Test the validity of a flag submission """
+    if flag.exact:
+        if submitted_flag == flag.value:
+            return True
+        elif flag.value in submitted_flag:
+            return True
     return False
 
+
 def current_solve_count(flag):
-    valid_solves = Solve.objects.filter(flag=flag, correct=True)
+    """ Get the count of successful solves for a specific flag """
+    valid_solves = Solve.objects.filter(challenge=flag.challenge).filter(correct=True)                                  # find all correct submission for the associtated challenge
     return valid_solves.count()
 
 
 def determine_flag_value(flag, solve_count):
-    return 100
+    """ Calculate the points value for a successful solve """
+    value = flag.challenge.value_start
+    decay_range = flag.challenge.value_start - flag.challenge.value_soft_floor
+
+    decayed = decay_range / solve_count if solve_count else 0
+    value = value - decayed
+    return value
+
 
 def process_submission(flag_id, submitted_flag, user):
+    """ Handle the submission of a flag attempt, return a solve record """
     try:
-        if user.is_anonymous:                                                                                         # submission from an unauthenticated user...
+        if user.is_anonymous:                                                                                           # submission from an unauthenticated user...
             user = None
 
         flag = BaseFlag.objects.get(pk=flag_id)                                                                         # get the flag object from its ID
