45b55054dacfb4e2bc659d41316c57bee54315ff
OperationXen <OperationXen@apocryphasecurity.com>
2019-08-09 17:39:54 +0100

    Added actiontypes for game control

--- react_app/src/actions/actionTypes.js
+++ react_app/src/actions/actionTypes.js
@@ -8,3 +8,9 @@ export const UPDATE_NEW_USER = "UPDATE_NEW_USER";
 
 /* Flag submission actions */
 export const SUBMIT_FLAG = "SUBMIT_FLAG";
+
+
+/* Game control actions */
+export const GET_GAME_STATE = "GET_GAME_STATE"
+export const OPEN_CHALLENGE = "OPEN_CHALLENGE"
+export const CLOSE_CHALLENGE = "CLOSE_CHALLENGE"
--- react_app/src/actions/game.actions.js
+++ react_app/src/actions/game.actions.js
@@ -5,6 +5,18 @@ import gql from "graphql-tag";
 
 import ApolloClient from "apollo-boost";
 
+function getGameState(response) {
+    return {type: ActionTypes.GET_GAME_STATE, response};
+}
+
+function openChallenge(data) {
+  return { type: ActionTypes.OPEN_CHALLENGE, data };
+}
+
+function closeChallenge(data) {
+  return { type: ActionTypes.CLOSE_CHALLENGE, data };
+}
+
 const client = new ApolloClient({
   uri: "/graphql/"
 });
@@ -23,15 +35,18 @@ const FLAG_SUBMISSION_MUTATION = gql`
 
 export function submitFlag(flagid, flagAttempt) {
   client
-    .mutate({ mutation: FLAG_SUBMISSION_MUTATION, variables: {id: flagid, flag: flagAttempt}})
+    .mutate({
+      mutation: FLAG_SUBMISSION_MUTATION,
+      variables: { id: flagid, flag: flagAttempt }
+    })
     .then(result => {
-        if (result.data.submitflag.result.correct){
-            alert("correct: " + result.data.submitflag.result.value )
-        } else {
-            alert("incorrect")
-        }
+      if (result.data.submitflag.result.correct) {
+        alert("correct: " + result.data.submitflag.result.value);
+      } else {
+        alert("incorrect");
+      }
     })
     .finally();
 
-    return;
+  return;
 }
