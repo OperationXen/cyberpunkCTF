c86f8658c9f5b0197ed46865584154ee4b726311
OperationXen <OperationXen@apocryphasecurity.com>
2019-10-27 10:54:23 +0000

    Updated all dependencies and added react router

--- react_app/src/components/ProfileButton.js
+++ react_app/src/components/ProfileButton.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { withRouter } from "react-router";
 
 import Button from "@material-ui/core/Button";
 import { logout, updateNewUser } from "actions/auth.actions";
@@ -14,29 +16,33 @@ class ProfileButton extends React.Component {
 
   /* When existing user wants to log in not register */
   existingUser() {
-    this.props.updateNewUser(false);
+    this.props.history.push("/login");
+    //this.props.updateNewUser(false);
   }
 
   /* New user wishes to sign up */
   newUser() {
-    this.props.updateNewUser(true);
+    this.props.history.push("/register");
+    //this.props.updateNewUser(true);
   }
 
   render() {
     if (!this.props.isAuthenticated) {
-      if (this.props.newUser) {
-        return (
-          <Button color="inherit" onClick={this.existingUser}>
-            Log In
-          </Button>
-        );
-      } else {
-        return (
-          <Button color="inherit" onClick={this.newUser}>
-            Register
-          </Button>
-        );
-      }
+      return (
+        <Switch>
+          <Route path="/login">
+            <Button color="inherit" onClick={this.newUser}>
+              Register Account
+            </Button>
+          </Route>
+          <Route path="/register">
+            <Button color="inherit" onClick={this.existingUser}>
+              Login Existing User
+            </Button>
+          </Route>
+          <Route render={() => <Redirect to="/login" />} />
+        </Switch>
+      );
     } else {
       return (
         <Button color="inherit" onClick={() => this.props.logout()}>
@@ -56,7 +62,10 @@ const mapDispatchToProps = dispatch => ({
   updateNewUser: val => dispatch(updateNewUser(val)),
   logout: () => dispatch(logout())
 });
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProfileButton);
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(ProfileButton)
+);
--- react_app/src/components/TitleBar.js
+++ react_app/src/components/TitleBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Typography from "@material-ui/core/Typography";
@@ -42,7 +43,9 @@ export default function TitleBar(props) {
             {props.title}
           </Typography>
 
-          <ProfileButton />
+          <Router>
+            <ProfileButton />
+          </Router>
         </Toolbar>
       </AppBar>
     </div>
